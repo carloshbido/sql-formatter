03510108b5921048e7c86b0cbd99cd081f912032
Carlos Bido <carloshbido@gmail.com>
2023-06-29 15:21:39 -0300

    feat: consoles to verify error

--- components/Cookie.tsx
+++ components/Cookie.tsx
@@ -26,9 +26,9 @@ function Cookie() {
       setMounted(false)
       try {
         await userInfo()
-        return
       }catch(err) {
-        console.log("erro ao cadastrar dados no Banco")
+        console.log(err)
+        console.log("Erro ao cadastrar dados no Banco")
       }
     }
   }
@@ -36,15 +36,14 @@ function Cookie() {
   return (mounted  && !localData)
   ? createPortal(
     <div className="fixed h-full top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-slate-400 px-10 bg-opacity-75" >
-      <div className="p-10 bg-slate-100 flex flex-col rounded-lg gap-3 text-lg border-solid border-gray-200 border-2">
+      <div className="py-10 px-4 bg-slate-100 flex flex-col rounded-lg gap-3 text-sm border-solid border-gray-200 border-2 md:text-lg md:p-10">
       <div>
         We use cookies to improve your experience on our site and to show you personalized ads. Select "Accept all" to accept all cookies. We never share your data.
       </div>
-      <div className="flex items-center justify-end gap-10" onClick={handleAcceptAll}>
+      <div className="flex items-center justify-end gap-4 md:gap-10" onClick={handleAcceptAll}>
         <button>Cancel</button>
         <button 
-          className="bg-emerald-500 text-white py-2 px-4 rounded-lg border-solid border-emerald-500 border-2 duration-300 ease-in-out 
-                     hover:bg-white hover:text-emerald-500"
+          className="bg-emerald-500 text-white py-2 px-4 rounded-lg border-solid border-emerald-500 border-2 duration-300 ease-in-out hover:bg-white hover:text-emerald-500"
         >  
           Accept All
         </button>
